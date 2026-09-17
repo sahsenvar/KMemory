4f4152f5f0533f027496025ce7066d99d7d74876
Şahan Şenvar <104942766+sahsenvar@users.noreply.github.com>
2026-09-18 01:31:19 +0300

    chore: upstream ornek uygulamalarini kaldir

--- composeApp/src/commonMain/kotlin/io/github/semenciuccosmin/preferences/sample/data/preferences/SamplePreferencesConstructor.kt
+++
@@ -1,5 +0,0 @@
-package io.github.semenciuccosmin.preferences.sample.data.preferences
-
-import io.github.semenciuccosmin.preferences.factory.PreferencesConstructor
-
-expect object SamplePreferencesConstructor : PreferencesConstructor<SamplePreferences>
